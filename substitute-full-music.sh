e24ed5c600a59b845d77872cde4f5d118e0e1c9a
Caesar <gitter@liftm.de>
2018-03-11 13:09:22 +0100

    Do not package ala's music and download it directly from openclonk.org on install

--- substitute-full-music.sh
+++ substitute-full-music.sh
@@ -1,8 +1,15 @@
-set -e -u -x
+#!/bin/sh
+set -e -u
 
-mkdir full-music
-cd full-music
-mv ../full-Music.ocg Music.ocg
-../c4group Music.ocg -x
-cd Music.ocg
-mv * ../../planet/Music.ocg/
+if [ $PWD != '/app/extra' ]; then
+	echo Something about flatpak\'s extra-data mechanism changed, rewrite this script.
+	exit 255
+fi
+ls
+c4group Music.ocg -x
+for f in /app/share/games/openclonk/*.ocg; do
+	if [ -z ${f##*Music.ocg} ]; then
+		continue
+	fi
+	ln -s "$f" .
+done
